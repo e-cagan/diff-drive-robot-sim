a849a6687cda2c0ccf88fb10f093fe8d25bc2f05
e-cagan <emincaganapaydin@gmail.com>
2026-02-19 00:01:59 +0300

    Implemented the template of workspace.

---
+++ src/diffbot_gazebo/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'diffbot_gazebo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        # Adding additional directories
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),     # Launch
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*')),     # Worlds
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cagan',
+    maintainer_email='emincaganapaydin@gmail.com',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
