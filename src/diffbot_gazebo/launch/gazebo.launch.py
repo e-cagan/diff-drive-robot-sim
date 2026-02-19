48a2473839807b96758e5fdf93830603131cb6f3
e-cagan <emincaganapaydin@gmail.com>
2026-02-19 16:48:00 +0300

    Implemented gazebo launch file and ran gazebo simulator with our robot.

---
+++ src/diffbot_gazebo/launch/gazebo.launch.py
@@ -0,0 +1,56 @@
+"""
+Launch file for starting gazebo simulator along with robot description.
+"""
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    """
+    A function to generate launch description
+    """
+
+    return LaunchDescription([
+
+        # 1. Launch gazebo simulator
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+            )
+        ),
+
+        # 2. Launch our description launch file
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                os.path.join(get_package_share_directory('diffbot_description'), 'launch', 'description.launch.py')
+            ),
+            launch_arguments={'gui': 'false'}.items()
+        ),
+
+        # 3. Spawn the entitiy
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            arguments=[
+                '-entity', 'diffbot',
+                '-topic', 'robot_description'
+            ],
+            output='screen',
+        ),
+
+        # 4. Run the teleop_twist_keyboard node to control robot manually
+        Node(
+            name='teleop_twist_keyboard_node',
+            package='teleop_twist_keyboard',
+            executable='teleop_twist_keyboard',
+            output='screen',
+            prefix='xterm -e',
+        )
+
+    ])
